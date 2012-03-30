321fcb37b4a3c53dfb7e1faa858cb947488c7046
Roman Kuznietsov <roman@hospital-systems.com>
2012-03-30 08:32:53 +0300

    change search area resize logic

--- main.cpp
+++ main.cpp
@@ -4,79 +4,74 @@
 #include <opencv2/objdetect/objdetect.hpp>
 
 #include <vector>
+#include <iostream>
 
 #define WINDOW_NAME "eye pointer"
-#define FACE_CASCADE_FILE "haarcascades/haarcascade_frontalface_alt.xml"
-#define EYE_CASCADE_FILE "haarcascades/haarcascade_mcs_eyepair_big.xml"
+#define FACE_SEARCH_WIDTH 160
+#define FACE_SEARCH_HEIGHT 120
 
-int main() {
+int main()
+{
     cv::VideoCapture camera(0);
     assert( camera.isOpened() );
 
     cv::CascadeClassifier faceCascade;
-    assert( faceCascade.load(FACE_CASCADE_FILE) );
+    assert( faceCascade.load("haarcascades/haarcascade_frontalface_alt.xml") );
 
     cv::CascadeClassifier eyeCascade;
-    assert( eyeCascade.load(EYE_CASCADE_FILE) );
+    assert( eyeCascade.load("haarcascades/haarcascade_mcs_eyepair_big.xml") );
 
     cv::Mat frame, image, faceImage;
     cv::namedWindow( WINDOW_NAME, 1 );
-
     std::vector<cv::Rect> objects;
     cv::Rect faceRect, eyeRect;
+    float faceModX, faceModY, eyeModX, eyeModY;
 
-    do {
-        //get frame from camera
+    camera >> frame;
+    faceModX = frame.cols / FACE_SEARCH_WIDTH;
+    faceModY = frame.rows / FACE_SEARCH_HEIGHT;
+
+    do
+    {
         camera >> frame;
 
-        // resize image to decrese processing time
-        cv::resize(frame, image, cv::Size(160, 120));
-
-        // detect face
+        cv::resize(frame, image, cv::Size(FACE_SEARCH_WIDTH, FACE_SEARCH_HEIGHT));
         faceCascade.detectMultiScale(image, objects);
 
-        if (objects.size()) { // if face found
+        if ( objects.size() )
+	{
             faceRect = objects[0];
 
-            // scale face rectangle to frame size
-            faceRect.x *= 4;
-            faceRect.y *= 4;
-            faceRect.height *=4;
-            faceRect.width *= 4;
+            faceRect.x *= faceModX;
+            faceRect.y *= faceModY;
+            faceRect.height *= faceModY;
+            faceRect.width *= faceModX;
 
-            // draw face rectangle
             cv::rectangle(frame, faceRect, CV_RGB(255, 0, 0));
 
-            // get face subimage
             faceImage = frame
                     .rowRange(cv::Range(faceRect.y, faceRect.y + faceRect.height))
                     .colRange(cv::Range(faceRect.x, faceRect.x + faceRect.width));
 
-            // resize image to decrease processing time
-            cv::resize(faceImage, image, cv::Size(faceRect.width / 2, faceRect.height / 2));
+            cv::resize(faceImage, faceImage, cv::Size(faceRect.width / 2, faceRect.height / 2));
+            eyeCascade.detectMultiScale(faceImage, objects);
 
-            // detect eyes
-            eyeCascade.detectMultiScale(image, objects);
-
-            if (objects.size()) { // if eyes founs
+            if ( objects.size() )
+	    {
                 eyeRect = objects[0];
 
-                // scale eyes rectangle to frame size
                 eyeRect.x *= 2;
                 eyeRect.y *= 2;
                 eyeRect.height *= 2;
                 eyeRect.width *= 2;
 
-                // offset eyes rectangle to draw it on main frame
                 eyeRect.x += faceRect.x;
                 eyeRect.y += faceRect.y;
 
-                //draw eyes rectangle
                 cv::rectangle(frame, eyeRect, CV_RGB(0, 255, 0));
             }
         }
 
-        //show frame
         cv::imshow( WINDOW_NAME, frame);
 
     } while (cv::waitKey(10) != 27); // 27 is ESC key
