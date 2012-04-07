d972d18a39607387654946f7486a7f680e2fd6fe
serzh <nechyporchuk@gmail.com>
2012-04-07 22:27:48 +0300

    add cvblob lib

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "helpers.h"
+#include <cvblob.h>
+
 #define WINDOW_NAME "eye pointer"
 #define WINDOW_RIGHT_EYE "Right eye"
 #define FACE_SEARCH_WIDTH 160
@@ -16,11 +18,19 @@ int main() {
 
 	cv::Mat frame, image, faceImage, eyeImage, rightEyeImage, leftEyeImage;
 	cv::namedWindow( WINDOW_NAME, 1 );
+	int threshold = 40;
+	cv::createTrackbar("threshold", WINDOW_NAME, &threshold, 255);
 	std::vector<cv::Rect> objects;
 	IntVec vHist, hHist, rightHorizHist, rightVertHist, leftHorizHist, leftVertHist;
 	cv::Rect faceRect, eyeRect, leftEye, rightEye;
 	float faceModX, faceModY, eyeModX, eyeModY;
 	int eyeCut;
+	cvb::CvBlobs blobs;
+	unsigned int result;
+	IplImage iplimg;
+	cvb::CvLabel largestL;
+	cvb::CvBlob* largest;
+
 
 	camera >> frame;
 	faceModX = frame.cols / FACE_SEARCH_WIDTH;
@@ -69,9 +79,19 @@ int main() {
 				cv::cvtColor(rightEyeImage, rightEyeImage, CV_BGR2GRAY);
 
 				cv::equalizeHist(rightEyeImage, rightEyeImage);
+				cv::threshold(rightEyeImage, rightEyeImage, threshold, 255, CV_THRESH_BINARY_INV);	
 				
-				cv::imshow( WINDOW_RIGHT_EYE, rightEyeImage);
+				iplimg = rightEyeImage;
+				IplImage *labels = cvCreateImage(rightEyeImage.size(), IPL_DEPTH_LABEL, 1);
+				result = cvb::cvLabel(&iplimg, labels, blobs);
 
+				largestL = cvb::cvGreaterBlob(blobs);
+				largest = blobs[largestL];
+				cv::rectangle(frame, cv::Point(eyeRect.x + largest->minx, eyeRect.y + largest->miny),
+					cv::Point(eyeRect.x + largest->maxx, eyeRect.y + largest->maxy),
+					CV_RGB(0,255,0));
+
+				cv::imshow( WINDOW_RIGHT_EYE, rightEyeImage);
 			}
 		}
 		cv::imshow( WINDOW_NAME, frame);
